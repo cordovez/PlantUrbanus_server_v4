eba4f65248a345b7b931dae5791003f88dd59edd
JC CorMan <cordovez@el-cordovez.com>
2023-03-21 17:46:32 +0100

    launch server script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PlantUrbanus_server_v4
+
 FastAPI / MongoDB / REST server
+
+### launching server:
+
+- python3 main.py
+- uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from routes.user_routes import user
+import uvicorn
 
 
 app = FastAPI()
 app.include_router(user)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        reload=True,
+        app="main:app"
+    )
